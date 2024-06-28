e908768a71e7221c4ebb8d37f4e518a34338d65e
gungurbuz <gunguneypro@gmail.com>
2024-06-28 18:50:48 +0300

    tests

---
+++ src/Helper.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class Helper {
+
+    public void init(){
+        try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://192.168.1.102:3306/mydb", "root", "1234");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+    public String login(String username, String passString){
+        
+    }
+    
+// try {
+//     Class.forName("com.mysql.cj.jdbc.Driver");
+//     Connection con = DriverManager.getConnection(
+//             "jdbc:mysql://192.168.1.102:3306/mydb", "root", "1234");
+//     // here sonoo is database name, root is username and password
+//     Statement stmt = con.createStatement();
+//     ResultSet rs = stmt.executeQuery("select * from member");
+//     while (rs.next())
+//         System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getInt(4));
+//     con.close();
+// } catch (Exception e) {
+//     System.out.println(e);
+// }
+    
+}
