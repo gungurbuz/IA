0f2e3dac80fbaba1c39b7e6f1413154aaab43854
gungurbuz <50180396+gungurbuz@users.noreply.github.com>
2024-07-23 15:33:52 +0300

    started work on user book input, changed ISBN conversion method

--- src/Library.java
+++ src/Library.java
@@ -1,35 +1,51 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.Scanner;
+import java.util.ArrayList;
+import org.apache.commons.validator.routines.ISBNValidator;
 
 public class Library {
-    public static String ISBN10toISBN13(String ISBN10) {
-        // original code taken from https://stackoverflow.com/a/29455307/15302425
-        String ISBN13 = ISBN10;
-        ISBN13 = "978" + ISBN13.substring(0, 9);
-        int d;
+    private static Scanner s = new Scanner(System.in);
+    private static String booktitle = "";
+    private static ArrayList<String> authors = new ArrayList<String>();
+    private static String ISBN= "";
+    private static String ISBN13 = "";
+    Connection con = App.getConnection();
 
-        int sum = 0;
-        for (int i = 0; i < ISBN13.length(); i++) {
-            d = ((i % 2 == 0) ? 1 : 3);
-            sum += ((((int) ISBN13.charAt(i)) - 48) * d);
+    public static void addBookPublic(){
+        boolean check = false;
+        System.out.println("Input book title:");
+        booktitle = s.nextLine();
+        Helper.clearConsole();
+        System.out.println("Input book author(s), press enter after each author, press enter on empty line to proceed");
+        do{
+            String tempinput = s.nextLine();
+            if(tempinput.equals("")){
+                check = true;
+            }
+            else{
+                authors.add(tempinput);
+            }
+            Helper.clearConsole();
         }
-        sum = 10 - (sum % 10);
-        String sumString = "";
-        if (sum == 10)
-            sumString = "X";
-        else
-            sumString += sum;
-        ISBN13 += sumString;
+        while(check == false);
+        System.out.println("Input ISBN without hypens or spaces");
+        ISBN = s.nextLine();
+        if (ISBN.length() == 10){ 
+            ISBN13 = ISBNValidator.getInstance().convertToISBN13(ISBN);
+        }
+        else ISBN13 = ISBN;
+        
+        
 
-        return ISBN13;
+
     }
 
-    //private void addBook(){
-    //    Connection con = App.getConnection();
-    //    try{
-    //        PreparedStatement addbookstmt = con.prepareStatement("INSERT INTO book (?, ? )")
-    //    } catch(Exception e){
-    //        e.printStackTrace();
-    //    }
-    //}
+    private void addBook() {
+        try {
+            PreparedStatement addbookstmt = con.prepareStatement("INSERT INTO book (?, ? )");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
