0fea87ebf5edbbf82d9baddfdda66d591afe6bb4
gungurbuz <50180396+gungurbuz@users.noreply.github.com>
2024-07-24 15:39:17 +0300

    added comments explaining code and continued work on language adding functionality

--- src/Library.java
+++ src/Library.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
-
 import org.apache.commons.validator.routines.ISBNValidator;
 
 public class Library {
@@ -15,7 +14,7 @@ public class Library {
     private static ArrayList<String> authors = new ArrayList<String>();
     private static String ISBN = "";
     private static String ISBN13 = "";
-    private static Connection con = App.getConnection();
+    private static Connection con = App.getConnection(); // get connection object created in app
 
     public static void addBookPublic() {
 
@@ -35,11 +34,12 @@ public class Library {
             Helper.clearConsole();
             System.out.println(
                     "Input book author(s), press enter after each author, press enter on empty line to proceed");
-        } while (check == false);
+        } while (check == false); // allow for multiple authors to be entered
         System.out.println("Input ISBN without hypens or spaces");
         ISBN = s.nextLine();
         if (ISBN.length() == 10) {
-            ISBN13 = ISBNValidator.getInstance().convertToISBN13(ISBN);
+            ISBN13 = ISBNValidator.getInstance().convertToISBN13(ISBN); // converts pre-2007 10 digit ISBN numbers to
+                                                                        // the current 13 digit standard
         } else {
             ISBN13 = ISBN;
         }
@@ -49,26 +49,34 @@ public class Library {
             HashMap<Integer, String> languageNames = new HashMap<Integer, String>();
             String tempLang = "";
             Helper.clearConsole();
-            System.out.println("Choose language from options below or add a new one");
+            System.out.println("Choose language from options below or enter 999 to add a new one");
             int i = 1;
             try {
                 Statement langstmt = con.createStatement();
                 ResultSet langstmtrs = langstmt.executeQuery("select * from language;");
-                while (langstmtrs.next()) {
+                while (langstmtrs.next()) { // prints language list for user to choose
                     tempLang = langstmtrs.getString("languagename");
                     System.out.print(i + ". ");
                     System.out.println(tempLang);
-                    languageNames.put(i, tempLang);
+                    languageNames.put(i, tempLang); // adds languages to hashmap to check against when making a choice
                     i = i + 1;
                 }
+                System.out.println("999. Add new language");
                 String langChoice = s.nextLine();
-                if(langChoice.equals(null) && !isLang){
+                if (langChoice.equals(null) && !isLang) {
                     System.out.println("Invalid input, try again.");
-                }
-                else if (Objects.nonNull(langChoice)) {
+                } else if (Objects.nonNull(langChoice)) {
                     int langId = Integer.parseInt(langChoice);
-                    if (languageNames.get(langId).equals(null)){
+                    if (languageNames.get(langId).equals(null) && langId != 999) { // checks if user input is a valid
+                                                                                   // language and not a new language
+                                                                                   // entry
                         System.out.println("Invalid input, please choose a language from the list.");
+                    } else if (languageNames.get(langId).equals(null) && langId == 999) {
+                        System.out.println("Type name of new language below and press enter:");
+                        String newLang = s.nextLine();
+                        PreparedStatement addlangstmt = con
+                                .prepareStatement("INSERT INTO language (languagename) VALUES (?);");
+                        addlangstmt.setString(1, newLang);
                     }
                 }
 
