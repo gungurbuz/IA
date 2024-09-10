0701652d4137812c94f22630883565c1053fb336
gungurbuz <50180396+gungurbuz@users.noreply.github.com>
2024-09-10 09:02:54 +0300

    added pookie

---
+++ src/main/java/applicationlayer/AlpYarkinDemir.java
@@ -0,0 +1,12 @@
+package applicationlayer;
+
+public class AlpYarkinDemir {
+	private int tool;
+	AlpYarkinDemir(int length){
+		tool = length;
+	}
+	
+	public int getToolLength(){
+		return tool;
+	}
+}
